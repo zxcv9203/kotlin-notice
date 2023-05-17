404e2c96eb6ba4af1193468b2c0b92dcf2cf49c7
yongckim <zxcv9203@naver.com>
2023-05-17 23:27:04 +0900

    :recycle: refactor : Member 엔티티 연관관계 추가 및 업데이트 메서드 추가

--- src/main/kotlin/com/example/notice/domain/post/converter/PostConverter.kt
+++ src/main/kotlin/com/example/notice/domain/post/converter/PostConverter.kt
@@ -1,10 +1,15 @@
 package com.example.notice.domain.post.converter
 
+import com.example.notice.domain.member.model.Member
 import com.example.notice.domain.post.controller.dto.request.PostSaveRequest
 import com.example.notice.domain.post.model.Post
 
 fun PostSaveRequest.toEntity(memberId: Long) = Post(
     title = this.title,
     content = this.content,
-    memberId = memberId
+    member = Member(
+        id = memberId,
+        name = this.memberName,
+        password = this.password
+    )
 )
